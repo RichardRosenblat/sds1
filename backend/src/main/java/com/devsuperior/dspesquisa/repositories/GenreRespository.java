4865c3923684710d6be4d01cfc0b1d4a053865ab
Richard <rosenblatr@gmail.com>
2020-09-17 18:13:46 -0300

    Entities, Repositories

---
+++ backend/src/main/java/com/devsuperior/dspesquisa/repositories/GenreRespository.java
@@ -0,0 +1,9 @@
+package com.devsuperior.dspesquisa.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.devsuperior.dspesquisa.entities.Genre;
+
+public interface GenreRespository extends JpaRepository<Genre, Long>{
+
+}
